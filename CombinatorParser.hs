03c2ab0ac46da1a8c0d4cfc032d53c18fa3703af
Yumen Cao <ycao10@illinois.edu>
2016-07-10 14:14:20 -0500

    added combinator parser

---
+++ CombinatorParser.hs
@@ -0,0 +1,21 @@
+module CombinatorParser where
+
+  newtype Parser t = Parser (String -> [(t,String)])
+  run (Parser p) = p
+
+  char s =
+    Parser (\inp -> case inp of
+      (x:xs) | s == x -> [(x,xs)]
+      otherwise       -> [])
+
+  oneOf xx =
+    Parser (\inp -> case inp of
+      (s:ss) | elem s xx -> [(s,ss)]
+      otherwise          -> [])
+
+  sat pred =
+    Parser (\inp -> case inp of
+      (s:ss) | pred s -> [(s,ss)]
+      otherwise       -> [])
+
+  digit = sat (\x -> x >= '0' && x<= '9')
